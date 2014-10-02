8122ede1e6158dd8ba18efab0339f562641ec9b3
Kelly Copley <copleykj@gmail.com>
2014-10-02 14:22:21 -0400

    Update package.js for new package system

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
+    name: "nerdmed:mediaqueries"
     summary: "Reactive media queries for all browsers"
+    version: "0.1.3",
+    git: "https://github.com/nerdmed/mediaqueries.git",
 });
 
-Package.on_use(function(api, where) {
+Package.onUse(function(api, where) {
+    api.versionsFrom("METEOR@0.9.3")
 
-    api.use(['reactive-dict', 'underscore', 'ejson', 'deps', 'ui'], 'client');
-    api.add_files(['lib/media-match/media.match.js', 'lib/mediaqueries.js'], 'client');
-
-    if (api.export)
-        api.export('mediaqueries');
+    api.use(['reactive-dict', 'underscore', 'ejson', 'deps', 'templating'], 'client');
+    api.addFiles(['lib/media-match/media.match.js', 'lib/mediaqueries.js'], 'client');
 });
