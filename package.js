68250b1dbca6e902914a41067adbd37a1c151840
Ahmed Hassan <a.hassan@buismed.de>
2014-12-28 02:00:47 +0100

    bump version dep

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: "nerdmed:mediaqueries",
     summary: "Reactive media queries for all browsers",
-    version: "0.1.3",
+    version: "0.1.4",
     git: "https://github.com/nerdmed/mediaqueries.git"
 });
 
 Package.onUse(function(api, where) {
-    api.versionsFrom("METEOR@0.9.3")
+    api.versionsFrom("1.0")
 
     api.use(['reactive-dict', 'underscore', 'ejson', 'deps', 'templating'], 'client');
     api.addFiles(['lib/media-match/media.match.js', 'lib/mediaqueries.js'], 'client');
